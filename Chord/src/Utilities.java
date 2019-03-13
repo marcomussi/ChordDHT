8f8717ada7ecbe2af7e316c285f3b8557a6271cf
Seve <giuseppe.severino@mail.polimi.it>
2019-03-13 15:52:35 +0100

    Added Utilities class

---
+++ Chord/src/Utilities.java
@@ -0,0 +1,43 @@
+import java.math.BigInteger; 
+import java.security.MessageDigest; 
+import java.security.NoSuchAlgorithmException; 
+
+public class Utilities {
+	// Method to calculate the SHA-1 of a given input string
+	public static String encryptString(String input) {
+        try { 
+            // getInstance() method is called with algorithm SHA-1 
+            MessageDigest md = MessageDigest.getInstance("SHA-1"); 
+  
+            // digest() method is called to calculate message digest of the input string 
+            // returned as array of byte 
+            byte[] messageDigest = md.digest(input.getBytes()); 
+  
+            // Convert byte array into signum representation 
+            BigInteger no = new BigInteger(1, messageDigest); 
+  
+            // Convert message digest into hex value 
+            String hashtext = no.toString(16); 
+  
+            // Add preceding 0s to make it 32 bit 
+            while (hashtext.length() < 32) { 
+                hashtext = "0" + hashtext; 
+            } 
+  
+            // return the HashText 
+            return hashtext; 
+        } 
+  
+        // For specifying wrong message digest algorithms 
+        catch (NoSuchAlgorithmException e) { 
+            throw new RuntimeException(e); 
+        } 
+
+	}
+	
+	public static void searchItem() {
+		// TODO Auto-generated method stub
+		// method in the main menu part will be developed here here
+	}
+
+}
